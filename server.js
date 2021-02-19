7d5c5cc2be57688cd6dabc70f17b45d5d13cb4e0
Lu-Songyu <songyulu99@gmail.com>
2021-02-19 01:21:53 -0800

    update

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require("express")
+const articleRouter = require('./routes/articles')
 const app = express()
 
 app.set('view engine', 'ejs')
 
+app.use('/articles', articleRouter)
+
 app.get('/', (req, res) =>{
-    res.render('index')
+    const articles = [
+        {
+            title: 'title 1',
+            publishDate: new Date(),
+            description: "This is the first example"
+        },
+        {
+            title: 'title 1',
+            publishDate: new Date(),
+            description: "This is the first example"
+        }
+    ]
+    res.render('articles/index', {articles: articles})
 })
 
 app.listen(0914)
