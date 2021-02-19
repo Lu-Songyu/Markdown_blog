3f0c6888137f2428acca99ffdee4882c25b4fc70
Lu-Songyu <songyulu99@gmail.com>
2021-02-19 12:32:44 -0800

    fix router

--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,13 @@
 const express = require('express')
+
 const router = express.Router()
 
 router.get('/new', (req, res) => {
     res.render('articles/new')
 })
 
+router.post('/', (rep, res) =>{
+
+})
 
 module.exports = router
